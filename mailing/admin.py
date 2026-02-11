356dc858e0ca82e06ef265a6aab7a385e54e522e
kirill.gladkikh <k.gladkikh@mail.ru>
2026-02-11 18:55:32 +0300

    mailing::admin.py::Client

--- mailing/admin.py
+++ mailing/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Client, Mailing, Message, Log
 
-# Register your models here.
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ['full_name', 'email', 'owner']
+    list_filter = ['owner']
+    search_fields = ['full_name', 'email']
+
+# @admin.register(Mailing)
+# class MailingAdmin(admin.ModelAdmin):
+#     list_display = ['id', 'start_datetime', 'status', 'owner']
+#     list_filter = ['status', 'owner']
+#     date_hierarchy = 'start_datetime'
+#
+# @admin.register(Message)
+# class MessageAdmin(admin.ModelAdmin):
+#     list_display = ['subject', 'owner']
+#     list_filter = ['owner']
+#
+# @admin.register(Log)
+# class LogAdmin(admin.ModelAdmin):
+#     list_display = ['datetime', 'status', 'mailing']
+#     list_filter = ['status', 'datetime']
+#     date_hierarchy = 'datetime'
