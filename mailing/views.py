98c787791aa6734cf021be6f9277f61cea3fe131
kirill.gladkikh <k.gladkikh@mail.ru>
2026-02-11 18:31:54 +0300

    mailing::views.py::Client

--- mailing/views.py
+++ mailing/views.py
@@ -1,5 +1,119 @@
-from django.shortcuts import render
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from django.contrib import messages
+from .models import Client, Mailing, Message, Log
+from .forms import ClientForm, MailingForm, MessageForm
 
-# Create your views here.
-def home(request):
-    return render(request, 'home.html')
+
+# Клиенты (Client)
+class ClientListView(ListView):
+    model = Client
+    template_name = 'clients_list.html'
+    context_object_name = 'clients'
+
+
+class ClientCreateView(CreateView):
+    model = Client
+    form_class = ClientForm
+    template_name = 'client_form.html'
+    success_url = reverse_lazy('client_list')
+
+    def form_valid(self, form):
+        messages.success(self.request, 'Клиент создан!')
+        return super().form_valid(form)
+
+
+class ClientUpdateView(UpdateView):
+    model = Client
+    form_class = ClientForm
+    template_name = 'client_form.html'
+    success_url = reverse_lazy('client_list')
+
+    def form_valid(self, form):
+        messages.success(self.request, 'Клиент обновлён!')
+        return super().form_valid(form)
+
+
+class ClientDeleteView(DeleteView):
+    model = Client
+    success_url = reverse_lazy('client_list')
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(request, 'Клиент удалён!')
+        return super().delete(request, *args, **kwargs)
+
+
+# # Рассылки (Mailing)
+# class MailingListView(ListView):
+#     model = Mailing
+#     template_name = 'mailings_list.html'
+#     context_object_name = 'mailings'
+#
+#
+# class MailingCreateView(CreateView):
+#     model = Mailing
+#     form_class = MailingForm
+#     template_name = 'mailing_form.html'
+#     success_url = reverse_lazy('mailing_list')
+#
+#     def form_valid(self, form):
+#         messages.success(self.request, 'Рассылка создана!')
+#         return super().form_valid(form)
+#
+#
+# class MailingUpdateView(UpdateView):
+#     model = Mailing
+#     form_class = MailingForm
+#     template_name = 'mailing_form.html'
+#     success_url = reverse_lazy('mailing_list')
+#
+#     def form_valid(self, form):
+#         messages.success(self.request, 'Рассылка обновлена!')
+#         return super().form_valid(form)
+#
+#
+# class MailingDeleteView(DeleteView):
+#     model = Mailing
+#     success_url = reverse_lazy('mailing_list')
+#
+#     def delete(self, request, *args, **kwargs):
+#         messages.success(request, 'Рассылка удалена!')
+#         return super().delete(request, *args, **kwargs)
+#
+#
+# # Сообщения (Message)
+# class MessageListView(ListView):
+#     model = Message
+#     template_name = 'messages_list.html'
+#     context_object_name = 'messages'
+#
+#
+# class MessageCreateView(CreateView):
+#     model = Message
+#     form_class = MessageForm
+#     template_name = 'message_form.html'
+#     success_url = reverse_lazy('message_list')
+#
+#     def form_valid(self, form):
+#         messages.success(self.request, 'Сообщение создано!')
+#         return super().form_valid(form)
+#
+#
+# class MessageUpdateView(UpdateView):
+#     model = Message
+#     form_class = MessageForm
+#     template_name = 'message_form.html'
+#     success_url = reverse_lazy('message_list')
+#
+#     def form_valid(self, form):
+#         messages.success(self.request, 'Сообщение обновлено!')
+#         return super().form_valid(form)
+#
+#
+# class MessageDeleteView(DeleteView):
+#     model = Message
+#     success_url = reverse_lazy('message_list')
+#
+#     def delete(self, request, *args, **kwargs):
+#         messages.success(request, 'Сообщение удалено!')
+#         return super().delete(request, *args, **kwargs)
