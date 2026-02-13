12437fb8abe815c8c89bcd7d30e2502c454ba8b7
kirill.gladkikh <k.gladkikh@mail.ru>
2026-02-13 15:39:13 +0300

    mailing::admin.py:Mailing fix

--- mailing/admin.py
+++ mailing/admin.py
@@ -17,9 +17,9 @@ class MessageAdmin(admin.ModelAdmin):
 
 @admin.register(Mailing)
 class MailingAdmin(admin.ModelAdmin):
-    list_display = ['id', 'start_datetime', 'status', 'owner']
+    list_display = ['id', 'start_time', 'status', 'owner']
     list_filter = ['status', 'owner']
-    date_hierarchy = 'start_datetime'
+    date_hierarchy = 'start_time'
 
 
 # @admin.register(Log)
