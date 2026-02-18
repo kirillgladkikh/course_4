b49498e52c37a753680b481f85555eb2df4019a0
Kirill Gladkikh <k.gladkikh@mail.ru>
2026-02-18 15:11:11 +0300

    users::mail confirm & verification work!

--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from users.apps import UsersConfig
 from users.views import custom_login
 
-from users.views import UserCreateView  #, email_verification
+from users.views import UserCreateView, email_verification
 # from users.forms import LoginForm
 from . import views
 
@@ -18,5 +18,5 @@ urlpatterns = [
     # path("logout/", LogoutView.as_view(next_page='/'), name="logout"),
     path("logout/", LogoutView.as_view(), name="logout"),
     path("register/", UserCreateView.as_view(), name="register"),
-    # path("email-confirm/<str:token>/", email_verification, name="email-confirm"),
+    path("email-confirm/<str:token>/", email_verification, name="email-confirm"),
 ]
--- users/views.py
+++ users/views.py
@@ -37,27 +37,27 @@ class UserCreateView(CreateView):
     form_class = UserRegisterForm
     success_url = reverse_lazy("users:login")
 
-    # def form_valid(self, form):
-    #     user = form.save()
-    #     user.is_active = False
-    #     token = secrets.token_hex(16)
-    #     user.token = token
-    #     user.save()
-    #
-    #     host = self.request.get_host()
-    #     url = f"http://{host}/users/email-confirm/{token}/"
-    #
-    #     send_mail(
-    #         subject="Подтверждение почты",
-    #         message=f"Привет, перейди по ссылке для подтверждения почты {url}",
-    #         from_email=EMAIL_HOST_USER,
-    #         recipient_list=[user.email],
-    #     )
-    #     return super().form_valid(form)
+    def form_valid(self, form):
+        user = form.save()
+        user.is_active = False
+        token = secrets.token_hex(16)
+        user.token = token
+        user.save()
+
+        host = self.request.get_host()
+        url = f"http://{host}/users/email-confirm/{token}/"
+
+        send_mail(
+            subject="Подтверждение почты",
+            message=f"Привет, перейди по ссылке для подтверждения почты {url}",
+            from_email=EMAIL_HOST_USER,
+            recipient_list=[user.email],
+        )
+        return super().form_valid(form)
 
 
-# def email_verification(request, token):
-#     user = get_object_or_404(User, token=token)
-#     user.is_active = True
-#     user.save()
-#     return redirect(reverse("users:login"))
+def email_verification(request, token):
+    user = get_object_or_404(User, token=token)
+    user.is_active = True
+    user.save()
+    return redirect(reverse("users:login"))
