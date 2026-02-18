3d776998b5a85cc49dca7a7ab4b4dbfbeb0b2473
Kirill Gladkikh <k.gladkikh@mail.ru>
2026-02-18 13:11:47 +0300

    users::login/logout work! w django 6.0.2!

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 from users.apps import UsersConfig
+from users.views import custom_login
 
 # from users.views import UserCreateView, email_verification
 # from users.forms import LoginForm
@@ -9,8 +10,10 @@ from . import views
 app_name = UsersConfig.name  # 'users'
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path('login/', LoginView.as_view(template_name="login.html"), name='login'),
+    # path("", views.index, name="index"),
+    path('login/', custom_login, name='login'),
+    # path('login/', LoginView.as_view(template_name="login.html", redirect_authenticated_user=True, ), name='login'),
+    # path('login/', LoginView.as_view(template_name="login.html"), name='login'),
     # path("login/", LoginView.as_view(authentication_form=LoginForm, template_name="login.html"), name="login"),
     # path("logout/", LogoutView.as_view(next_page='/'), name="logout"),
     path("logout/", LogoutView.as_view(), name="logout"),
--- users/views.py
+++ users/views.py
@@ -1,9 +1,32 @@
-from django.shortcuts import render
-
-# Create your views here.
-# TEST
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
 
 
-def index(request):
-    return HttpResponse("Привет из приложения users!")
+def custom_login(request):
+    if request.user.is_authenticated:
+        return redirect('mailing:home_view')  # перенаправление на главную
+
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('mailing:home_view')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
+
+# from django.shortcuts import render
+#
+# # Create your views here.
+# # TEST
+# from django.http import HttpResponse
+#
+#
+# def index(request):
+#     return HttpResponse("Привет из приложения users!")
